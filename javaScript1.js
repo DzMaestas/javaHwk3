39eb50ead06e6ac7b54bc76613a15a08749f63a4
DzMaestas <dvd.maestas@gmail.com>
2021-10-09 13:55:54 -0600

    final1

--- javaScript1.js
+++ javaScript1.js
@@ -1,9 +1,36 @@
+"use strict"
 var number1 = 8;
 var number2 = 2;
 //console.log(number1);
 var sum= (number1+number2);
 console.log(sum);
+
 var x=8;
 var y = 2;
 var difference =  x-y;
 console.log(difference);
+
+var x=8;
+var y=2;
+var quotient=8/2;
+console.log(quotient);
+
+var x = 8;
+var y = 2;
+var product = x * y;
+console.log(product);
+
+var word = "Hello";
+var word1 = "World";
+var word2 = word + " " + word1;
+console.log(word2);
+
+var favFood = "My favorite food is";
+var food = " Mexican";
+var food2 = favFood + food;
+console.log(food2);
+
+
+
+
+
